917ccbd124dcbab2811175f743cddb5525679bce
robinhaak <60131820+robinhaak@users.noreply.github.com>
2026-05-11 13:38:56 +0200

    Removed obsolete unique() / np.unique() from getRelSpikeTimes / get_rel_spike_times

--- matlab/dependencies/getRelSpikeTimes.m
+++ matlab/dependencies/getRelSpikeTimes.m
@@ -15,6 +15,8 @@ function [relSpikeTimes,spikesPerEvent] = getRelSpikeTimes(spikeTimes,eventTimes
 %   v0.9 - 6 January 2025
 %   - created by Robin Haak
 %   v1.0 - 30 June 2025
+%   v1.0.1 - 11 May 2025
+%   - removed unique() call from artificial spikes block
 
 %% prep
 %ensure correct orientation
